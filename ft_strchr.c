4a23baf06e45b83dca15bc2daa683e1cdfa58308
Lucie <gnosticstate@gmail.com>
2025-04-29 23:11:09 +0200

    add ft_strchr | TODO: norminette

---
+++ ft_strchr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 21:09:41 by lucpardo          #+#    #+#             */
+/*   Updated: 2025/04/29 23:10:15 by lucpardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+		{
+			return ((char *)str + i);
+		}
+		++i;
+	}
+	if (str[i] == '\0' && c == '\0')
+	{
+		return ((char *)str + i);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+
+/*#include <stdio.h>
+int	main(void)
+{
+	printf("%s \n", ft_strchr("hello", 'o'));
+	return (0);
+}*/
