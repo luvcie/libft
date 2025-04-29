2619d28c0a13d5938632d48aaf6ec6b8437626ef
Lucie <gnosticstate@gmail.com>
2025-04-29 19:44:39 +0200

    now it calls isalpha and isdigit

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,17 +6,14 @@
 /*   By: lucpardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 17:29:12 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/04/28 19:01:13 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/04/29 19:43:06 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-int	ft_isalnum(int param)
+int		ft_isalnum(int param)
 {
-	if ((param >= 'a' && param <= 'z') || (param >= 'A' && param <= 'Z'))
-	{
-		return (1);
-	}	
-	else if (param >= '0' && param <= '9')
+	if (ft_isalpha(param) || ft_isdigit(param))
 	{
 		return (1);
 	}
@@ -25,9 +22,9 @@ int	ft_isalnum(int param)
 		return (0);
 	}
 }
-/*
-#include <stdio.h>
-int	main(void)
+
+/*#include <stdio.h>
+int		main(void)
 {
 	printf("%d \n", ft_isalnum('3'));
 	printf("%d \n", ft_isalnum('a'));
