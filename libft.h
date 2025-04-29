b4d35b5426e6559235cdee4bf874ebf16df185c9
Lucie <gnosticstate@gmail.com>
2025-04-29 12:13:50 +0200

    add libft.h

---
+++ libft.h
@@ -0,0 +1,13 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <strings.h>
+
+int		ft_isalpha(int letter);
+int		ft_isdigit(int digit);
+int		ft_isalnum(int param);
+int		ft_isascii(int ascii);
+int		ft_isprint(int value);
+size_t	ft_strlen(const char *str);
+
+#endif
