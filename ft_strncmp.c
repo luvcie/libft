48b382c3c14ce543ec7a9ec3c87d7d00a21d2f4e
luvcie <lucpardo@student.42.fr>
2025-05-01 21:11:07 +0200

    fix strlcpy

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 16:11:48 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/04/30 17:23:07 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/05/01 21:09:02 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -31,6 +31,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 		i++;
+	}
+	if (i == n)
+	{
+		return (0);
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
