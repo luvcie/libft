fb6756c57aaffae8d6a338501838484d721126fc
luvcie <lucpardo@student.42.fr>
2025-05-03 00:27:52 +0200

    add memcpy | WIP

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/02 19:47:37 by lucpardo          #+#    #+#             */
+/*   Updated: 2025/05/02 21:53:34 by lucpardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;	
+	unsigned char	*dstptr;
+	const unsigned char	*srcptr;
+
+	if (n == 0)
+		return ()
+
+
+	return (dst);
+}
