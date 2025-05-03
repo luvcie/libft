a23cfa040d75297c6ecd41528542459c41de863c
luvcie <lucpardo@student.42.fr>
2025-05-03 22:12:27 +0200

    remove limits.h and define INT_MAX and INT_MIN

--- libft.h
+++ libft.h
@@ -6,15 +6,16 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 18:47:40 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/05/03 21:37:00 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/05/03 22:11:55 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
 # define LIBFT_H
+# define INT_MAX 2147483647
+# define INT_MIN -2147483648
 
 # include <strings.h>
 # include <stdlib.h>
-# include <limits.h>
 
 int		ft_isalpha(int letter);
 int		ft_isdigit(int digit);
@@ -36,5 +37,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
