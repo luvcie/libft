215b723d3d41d9042c0896eba83f2e0bafec78c8
luvcie <lucpardo@student.42.fr>
2025-05-04 02:52:12 +0200

    add ft_substr | WIP

---
+++ ft_substr.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 02:48:41 by lucpardo          #+#    #+#             */
+/*   Updated: 2025/05/04 02:51:57 by lucpardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	ogstrlen;
+	size_t	substrlen;
+	size_t	i;
+}
