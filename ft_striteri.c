6259922b325357c022157f970a7e85515b74e3dd
luvcie <lucpardo@student.42.fr>
2025-05-05 19:02:33 +0200

    fix: return original char for non-uppercase chars

---
+++ ft_striteri.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 16:36:59 by lucpardo          #+#    #+#             */
+/*   Updated: 2025/05/05 16:37:01 by lucpardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+char	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+
+
+}
+// c: character to output
+// fd: the file descriptor on which to write.
+// external fs: write()
+// descr: outputs the character c to the specified file descriptor
