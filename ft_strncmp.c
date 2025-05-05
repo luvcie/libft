77902ba43d74b6702946fda22bde44e83fcad528
luvcie <lucpardo@student.42.fr>
2025-05-05 20:39:08 +0200

    fix: remove NULL Check to mimic libc crash behavior todo: add back after being evaluated

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -15,14 +15,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (0);
-	}
-	if (n == 0)
-	{
-		return (0);
-	}
 	i = 0;
 	while (i < n && s1[i] != '\0' && s2[i] != '\0')
 	{
