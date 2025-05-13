b8241efe53ad01d8adfe69c01bbdc55ea54ac331
luvcie <lucpardo@student.42.fr>
2025-05-14 01:28:25 +0200

    complete lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,14 +6,35 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 22:44:04 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/05/13 22:46:31 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/05/14 01:06:18 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new_node;
+	t_list	*new_lst;
+	void	*new_content;
 
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	new_lst = NULL;
+	while (lst != NULL)
+	{
+		new_content = f(lst->content);
+		new_node = ft_lstnew(new_content);
+		if (new_node == NULL)
+		{
+			if (del != NULL)
+				del(new_content);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
 // iterates through list 'lst', applies function 'f'
 // to each node's content. And creates a new list resulting
