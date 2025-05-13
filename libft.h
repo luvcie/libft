2c88f05110f9da93823e0d998d7abc5dec65caaf
luvcie <lucpardo@student.42.fr>
2025-05-14 01:27:55 +0200

    fix lstmap | TODO: fix line_too_long norm error

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 18:47:40 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/05/14 01:12:33 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/05/14 01:27:06 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -60,15 +60,14 @@ char				*ft_strjoin(char const *s1, char const *s2);
 void				ft_putnbr_fd(int n, int fd);
 char				*ft_itoa(int nb);
 
-t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
-int					ft_lstsize(t_list *lst);
-t_list				*ft_lstlast(t_list *lst);
-void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
-						void (*del)(void *));
+void				ft_lstadd_back(t_list **lst, t_list *new);
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list				*ft_lstlast(t_list *lst);
+t_list				*ft_lstnew(void *content);
+int					ft_lstsize(t_list *lst);
 
 #endif
