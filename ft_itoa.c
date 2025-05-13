90cc5f8fba22139704de8f61e7f7d4fb997c79d2
luvcie <lucpardo@student.42.fr>
2025-05-13 16:08:53 +0200

    add itoa | WIP

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,18 +6,34 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 12:06:02 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/05/05 12:19:07 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/05/13 16:08:38 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	ft_itoa(int nb)
+char	ft_itoa(int nb)
 {
 	char	c;
 	long	nbl;
 
+	nbl = nb;
 	if (nbl < 0)
 	{
-		
+		write(1, "-", 1);
+		nbl = -nbl;
 	}
+
+	if (nbl > 9)
+	ft_itoa(nbl/10);
+	c = (nbl % 10) + '0';
+	write(1, &c, 1);
+	return (nbl);
 }
+
+/*#include <stdio.h>
+int main(void)
+{
+	ft_itoa(1024);
+	ft_itoa(-1024);
+	return (0);
+}*/
