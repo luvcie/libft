7383a55e9a91e11728c6293d26176888056a7162
luvcie <lucpardo@student.42.fr>
2025-05-13 22:40:29 +0200

    add ft_lstiter

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,12 +6,25 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 23:57:05 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/05/08 23:58:19 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/05/13 22:38:39 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	ft_lstier(t_list *lst, void (*f)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
+	t_list	*node;
 
+	if (lst == NULL || f == NULL)
+		return ;
+	node = lst;
+	while (node != NULL)
+	{
+		f(node->content);
+		node = node->next;
+	}
 }
+// iterates through list 'lst' and applies function f
+// to content of each node.
+// lst: address of pointer to a node
+// f: address of function to apply to each node's content
