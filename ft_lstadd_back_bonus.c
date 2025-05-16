92cd67b45c97fc21c0e2a4e7563958d400c3a9bf
luvcie <lucpardo@student.42.fr>
2025-05-16 22:22:31 +0200

    add description

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+// adds node to end of linked list
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
